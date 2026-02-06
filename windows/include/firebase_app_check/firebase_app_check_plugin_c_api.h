c6909c560d6b73cb65812c765e478ebbb49e8726
Claw (Bot) <ben-claw@res.app>
2026-02-06 21:00:09 +1100

    Fix Windows registrar include

--- windows/include/firebase_app_check/firebase_app_check_plugin_c_api.h
+++ windows/include/firebase_app_check/firebase_app_check_plugin_c_api.h
@@ -1,7 +1,7 @@
 #ifndef FLUTTER_PLUGIN_FIREBASE_APP_CHECK_PLUGIN_C_API_H_
 #define FLUTTER_PLUGIN_FIREBASE_APP_CHECK_PLUGIN_C_API_H_
 
-#include <flutter_plugin_registrar_windows.h>
+#include <flutter_plugin_registrar.h>
 
 #ifdef FLUTTER_PLUGIN_IMPL
 #define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
